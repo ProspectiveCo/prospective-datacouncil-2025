b09921fddecdcc630327c4ed26e7aeb119b59a99
xdatanomad <parham.parvizi@gmail.com>
2025-04-21 16:50:09 -0700

    switching to the larger 2020-2024 data file.

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import { conn, db } from "./duckdb.js";
 import { client } from "./perspective.js";
 import { getPredicates } from "./query.js";
 
-const GENERATORS_URL = "https://perspective-demo-dataset.s3.us-east-1.amazonaws.com/pudl/generators_monthly_2022-2023_lg.parquet";
+
+const PLANTS_URL = "https://perspective-demo-dataset.s3.us-east-1.amazonaws.com/pudl/generators_monthly_2020-2023.parquet";
 
 
 function setStatus(msg = "Ready", showSpinner = false) {
@@ -18,7 +19,7 @@ document.querySelector("#load").addEventListener("click", async (event) => {
         setStatus("Loading DuckDB...", true);
         await conn.query(`
             CREATE TABLE generators AS
-            SELECT * FROM '${GENERATORS_URL}'
+            SELECT * FROM '${PLANTS_URL}'
         `);
         const numRows = (await conn.query("SELECT COUNT(*) as 'numrows' FROM generators")).get(0)['numrows'];
         setStatus(`DuckDB loaded: ${numRows} rows`, false);
@@ -34,28 +35,25 @@ document.querySelector("#query").addEventListener("click", async () => {
         setStatus(`Running query...`, true);
         const sql = `
             SELECT 
-                plant_name_eia as plant_name, report_date, 
-                city, state, county, latitude, longitude, timezone,
-                net_generation_mwh, capacity_mw, 
-                energy_source, fuel_type, technology_description, primary_mover
+                plant_name, report_date, 
+                city, state, county, lat, lon,
+                status_code, net_gen_mwh, capacity, 
+                fuel_type, tech_desc
             FROM generators
             ${getPredicates()}
             ORDER BY report_date;
         `;
         console.log("SQL:", sql);
         const result = await conn.query(sql);
+        const data = result.toArray();
         console.log(`db rows: ${result.numRows}`);
-
-        setStatus(`Loaded ${result.numRows} rows. Converting data...`, true);
-        const data = Array.from({ length: result.numRows }, (_, i) => {
-            const row = result.get(i);
-            return { ...row, report_date: new Date(row['report_date']).toISOString() };
-        });
+        
         let viewer = document.querySelector("#viewer");
         const table = await client.table(data, { format: "json" });
         const config = { ...await viewer.save(), settings: true, plugin_config: { edit_mode: "READ_ONLY" } };
         viewer.load(table);
         viewer.restore(config);
+
         setStatus(`Loaded ${data.length} rows.`, false);
     } catch (error) {
         setStatus("Failed to load table", false);
